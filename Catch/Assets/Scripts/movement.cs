bb64a8a85f1525f2f7fe75c6fb57c1ce0ff44eba
wikinto <41481527+wikinto@users.noreply.github.com>
2022-05-30 23:53:39 +0200

    all

---
+++ Catch/Assets/Scripts/movement.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class movement : MonoBehaviour
+{
+    Transform ply;
+    float x;
+    // Start is called before the first frame update
+    void Start()
+    {
+        ply = GetComponentInParent<Transform>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        x += Input.GetAxis("Horizontal") * 0.1f;
+        Debug.Log(Input.GetAxis("Horizontal"));
+        ply.transform.position = new Vector3(x,0,0);
+    }
+}
